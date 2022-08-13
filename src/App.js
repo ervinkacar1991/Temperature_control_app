222a3a0f56a8787da4b1d4e39caafd9887a26d35
ervinkacar1991 <kacarervin@gmail.com>
2022-08-13 12:16:14 +0200

    temperature control app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div className="app-container">
+      <div className="temperature-display-container">
+        <div className="temperature-display">10 C</div>
+      </div>
+      <div className="button-container">
+        <button>+</button>
+        <button>-</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
